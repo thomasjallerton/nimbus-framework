008acc02758ed063eb80fd978a1f02794913b3dd
thomasjallerton <Hood6wipe980>
2018-12-27 13:23:48 +0000

    Thought about how to parse

---
+++ src/main/java/parser/models/parsed/FunctionResource.kt
@@ -0,0 +1,12 @@
+package parser.models.parsed
+
+class FunctionResource: Resource() {
+
+    private val handler: String = ""
+    private val name: String = ""
+
+    private val envVariables: Map<String, String> = mapOf()
+
+    private val triggers: List<String> = listOf()
+
+}
