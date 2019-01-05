6636494a982fc8da165c352107e876b6ca9820ec
Thomas Allerton <thomasjallerton@gmail.com>
2019-01-05 13:13:50 +0000

    Can now use lambda proxy responses to build custom output

--- src/main/java/wrappers/models/LambdaProxyResponse.kt
+++ src/main/java/wrappers/models/LambdaProxyResponse.kt
@@ -2,6 +2,15 @@ package wrappers.models
 
 data class LambdaProxyResponse(
         var statusCode: Int = 200,
-        var headers: MutableMap<String, String> = mutableMapOf(),
-        var body: String = "",
-        var isBase64Encoded: Boolean = false)
+        var headers: Map<String, String> = mutableMapOf(),
+        var body: String = "{}",
+        var isBase64Encoded: Boolean = false) {
+
+    companion object {
+        fun serverErrorResponse(): LambdaProxyResponse {
+            return LambdaProxyResponse(502, mutableMapOf(), "{\"message\":\"server error\"}", false)
+        }
+    }
+}
+
+
