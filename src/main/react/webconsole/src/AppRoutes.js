929b44fda51b10abf7f3e12d8ec7f6597f33a1b1
thomasjallerton <Hood6wipe980>
2019-07-24 15:37:53 +0100

    Webconsole deployed as part of running the server

--- src/main/react/webconsole/src/AppRoutes.js
+++ src/main/react/webconsole/src/AppRoutes.js
@@ -18,22 +18,23 @@ function AppRoutes() {
             <div>
                 <NavBar/>
 
-                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/NimbusWebConsole/" component={HomePage}/>
+                <Route exact path="/NimbusWebConsole/index.html" component={HomePage}/>
 
-                <Route exact path="/Functions" component={FunctionsPage} />
+                <Route exact path="/NimbusWebConsole/Functions" component={FunctionsPage} />
 
-                <Route exact path="/FileBuckets" component={FileBucketsPage} />
-                <Route exact path="/FileBuckets/:bucketName" component={FileBucketPage}/>
+                <Route exact path="/NimbusWebConsole/FileBuckets" component={FileBucketsPage} />
+                <Route exact path="/NimbusWebConsole/FileBuckets/:bucketName" component={FileBucketPage}/>
 
-                <Route exact path="/DocumentStores" component={DocumentStoresPage} />
-                <Route exact path="/DocumentStores/:tableName" component={DocumentStorePage} />
+                <Route exact path="/NimbusWebConsole/DocumentStores" component={DocumentStoresPage} />
+                <Route exact path="/NimbusWebConsole/DocumentStores/:tableName" component={DocumentStorePage} />
 
-                <Route exact path="/KeyValueStores" component={KeyValueStoresPage} />
-                <Route exact path="/KeyValueStores/:tableName" component={KeyValueStorePage} />
+                <Route exact path="/NimbusWebConsole/KeyValueStores" component={KeyValueStoresPage} />
+                <Route exact path="/NimbusWebConsole/KeyValueStores/:tableName" component={KeyValueStorePage} />
 
-                <Route exact path="/Queues" component={QueuesPage} />
+                <Route exact path="/NimbusWebConsole/Queues" component={QueuesPage} />
 
-                <Route exact path="/Notifications" component={NotificationsPage} />
+                <Route exact path="/NimbusWebConsole/Notifications" component={NotificationsPage} />
             </div>
         </Router>
     );
