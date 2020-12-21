3cf2b189dc1c15363d5b50c652cb9b9dd7a293c0
Thomas Allerton <thomasjallerton@gmail.com>
2020-12-21 11:45:42 +0000

    Bump kotest version in nimbus-core

--- core/src/test/java/com/nimbusframework/nimbuscore/clients/queue/QueueIdAnnotationServiceTest.kt
+++ core/src/test/java/com/nimbusframework/nimbuscore/clients/queue/QueueIdAnnotationServiceTest.kt
@@ -3,8 +3,8 @@ package com.nimbusframework.nimbuscore.clients.queue
 import com.nimbusframework.nimbuscore.examples.queue.Queue
 import com.nimbusframework.nimbuscore.examples.queue.QueueNoStage
 import com.nimbusframework.nimbuscore.exceptions.InvalidStageException
-import io.kotlintest.shouldBe
-import io.kotlintest.specs.AnnotationSpec
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
 
 internal class QueueIdAnnotationServiceTest : AnnotationSpec() {
 
