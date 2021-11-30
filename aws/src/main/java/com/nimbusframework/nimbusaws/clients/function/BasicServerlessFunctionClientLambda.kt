6c0513d100ff7bcb11b1227c67a7429da2ec7aa2
Thomas Allerton <thomasjallerton@gmail.com>
2021-11-30 18:59:36 +0000

    switch over lambda

--- aws/src/main/java/com/nimbusframework/nimbusaws/clients/function/BasicServerlessFunctionClientLambda.kt
+++ aws/src/main/java/com/nimbusframework/nimbusaws/clients/function/BasicServerlessFunctionClientLambda.kt
@@ -1,32 +1,27 @@
 package com.nimbusframework.nimbusaws.clients.function
 
-import com.amazonaws.services.lambda.AWSLambda
-import com.nimbusframework.nimbusaws.cloudformation.resource.function.FunctionResource
-import com.amazonaws.services.lambda.AWSLambdaClientBuilder
-import com.amazonaws.services.lambda.model.InvocationType
-import com.amazonaws.services.lambda.model.InvokeRequest
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.inject.Inject
+import com.nimbusframework.nimbusaws.cloudformation.resource.function.FunctionResource
 import com.nimbusframework.nimbuscore.annotations.NimbusConstants
 import com.nimbusframework.nimbuscore.clients.function.BasicServerlessFunctionClient
 import com.nimbusframework.nimbuscore.clients.function.EnvironmentVariableClient
+import software.amazon.awssdk.core.SdkBytes
+import software.amazon.awssdk.services.lambda.LambdaClient
+import software.amazon.awssdk.services.lambda.model.InvocationType
+import software.amazon.awssdk.services.lambda.model.InvokeRequest
 import java.nio.charset.Charset
 
 
 internal class BasicServerlessFunctionClientLambda(
-        private val handlerClass: Class<out Any>,
-        private val functionName: String
-): BasicServerlessFunctionClient {
-
-    @Inject
-    private lateinit var lambdaClient: AWSLambda
-
-    @Inject
-    private lateinit var environmentVariableClient: EnvironmentVariableClient
+    private val handlerClass: Class<out Any>,
+    private val functionName: String,
+    private val lambdaClient: LambdaClient,
+    private val environmentVariableClient: EnvironmentVariableClient
+) : BasicServerlessFunctionClient {
 
     private val objectMapper = ObjectMapper()
 
-    private val projectName by lazy {  environmentVariableClient.get("NIMBUS_PROJECT_NAME") ?: "" }
+    private val projectName by lazy { environmentVariableClient.get("NIMBUS_PROJECT_NAME") ?: "" }
 
     private val stage by lazy { environmentVariableClient.get("FUNCTION_STAGE") ?: NimbusConstants.stage }
 
@@ -43,14 +38,15 @@ internal class BasicServerlessFunctionClientLambda(
     }
 
     override fun <T> invoke(param: Any, responseType: Class<T>): T? {
-        val invokeRequest = InvokeRequest()
-                .withFunctionName(FunctionResource.functionName(projectName, handlerClass.simpleName, functionName, stage))
-                .withPayload(objectMapper.writeValueAsString(param))
-                .withInvocationType(InvocationType.RequestResponse)
+        val invokeRequest = InvokeRequest.builder()
+            .functionName(FunctionResource.functionName(projectName, handlerClass.simpleName, functionName, stage))
+            .payload(SdkBytes.fromByteArray(objectMapper.writeValueAsBytes(param)))
+            .invocationType(InvocationType.REQUEST_RESPONSE)
+            .build()
         val result = lambdaClient.invoke(invokeRequest)
 
         return if (responseType != Unit.javaClass) {
-            val converted = String(result.payload.array(), Charset.forName("UTF-8"))
+            val converted = result.payload().asString(Charset.forName("UTF-8"))
             objectMapper.readValue(converted, responseType)
         } else {
             null
@@ -62,10 +58,11 @@ internal class BasicServerlessFunctionClientLambda(
     }
 
     override fun invokeAsync(param: Any) {
-        val invokeRequest = InvokeRequest()
-                .withFunctionName(FunctionResource.functionName(projectName, handlerClass.simpleName, functionName, stage))
-                .withPayload(objectMapper.writeValueAsString(param))
-                .withInvocationType(InvocationType.Event)
+        val invokeRequest = InvokeRequest.builder()
+            .functionName(FunctionResource.functionName(projectName, handlerClass.simpleName, functionName, stage))
+            .payload(SdkBytes.fromByteArray(objectMapper.writeValueAsBytes(param)))
+            .invocationType(InvocationType.EVENT)
+            .build()
         lambdaClient.invoke(invokeRequest)
     }
 }
