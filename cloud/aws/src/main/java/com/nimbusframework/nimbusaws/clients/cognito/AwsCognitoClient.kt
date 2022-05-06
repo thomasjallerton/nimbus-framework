3a1006d206a171d2e63034a16c88942bd9db887a
Thomas Allerton <thomasjallerton@gmail.com>
2022-05-06 09:58:17 +0100

    add cognito client tests

--- cloud/aws/src/main/java/com/nimbusframework/nimbusaws/clients/cognito/AwsCognitoClient.kt
+++ cloud/aws/src/main/java/com/nimbusframework/nimbusaws/clients/cognito/AwsCognitoClient.kt
@@ -1,12 +1,13 @@
 package com.nimbusframework.nimbusaws.clients.cognito
 
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminListGroupsForUserRequest
 import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminListGroupsForUserResponse
 
 class AwsCognitoClient(
     private val userPoolId: String,
     private val cognitoClient: CognitoIdentityProviderClient
-): CognitoClient {
+) : CognitoClient {
 
     override fun getUser(accessToken: String): CognitoUser {
         val response = cognitoClient.getUser { it.accessToken(accessToken) }
@@ -21,24 +22,30 @@ class AwsCognitoClient(
         var nextToken: String? = null
         val result = mutableListOf<String>()
         while (currentResponse == null || currentResponse.hasGroups()) {
-            currentResponse = cognitoClient.adminListGroupsForUser {
-                it
+            currentResponse = cognitoClient.adminListGroupsForUser(
+                AdminListGroupsForUserRequest.builder()
                     .userPoolId(userPoolId)
                     .username(username)
                     .nextToken(nextToken)
                     .limit(10)
-            }
+                    .build()
+            )
             nextToken = currentResponse!!.nextToken()
             result.addAll(currentResponse.groups().map { it.groupName() })
         }
         return result
     }
 
-    override fun searchUsers(filterAttribute: SearchableCognitoAttribute, value: String, searchType: SearchType): List<CognitoUser> {
+    override fun searchUsers(
+        filterAttribute: SearchableCognitoAttribute,
+        value: String,
+        searchType: SearchType
+    ): List<CognitoUser> {
         val searchTerm = "${filterAttribute.searchTerm} ${searchType.formatted} \"$value\""
-        val response = cognitoClient.listUsers { it
-            .userPoolId(userPoolId)
-            .filter(searchTerm)
+        val response = cognitoClient.listUsers {
+            it
+                .userPoolId(userPoolId)
+                .filter(searchTerm)
         }
         return response.users().map { user ->
             CognitoUser(
@@ -49,29 +56,33 @@ class AwsCognitoClient(
     }
 
     override fun addUserToGroupAsAdmin(username: String, groupName: String) {
-        cognitoClient.adminAddUserToGroup { it
-            .userPoolId(userPoolId)
-            .groupName(groupName)
-            .username(username)
+        cognitoClient.adminAddUserToGroup {
+            it
+                .userPoolId(userPoolId)
+                .groupName(groupName)
+                .username(username)
         }
     }
 
     override fun removeUserFromGroupAsAdmin(username: String, groupName: String) {
-        cognitoClient.adminRemoveUserFromGroup { it
-            .userPoolId(userPoolId)
-            .groupName(groupName)
-            .username(username)
+        cognitoClient.adminRemoveUserFromGroup {
+            it
+                .userPoolId(userPoolId)
+                .groupName(groupName)
+                .username(username)
         }
     }
 
     override fun setAttributeAsAdmin(username: String, attribute: String, value: String) {
-        cognitoClient.adminUpdateUserAttributes { it
-            .userPoolId(userPoolId)
-            .username(username)
-            .userAttributes({ builder -> builder
-                .value(attribute)
-                .name(value)
-            })
+        cognitoClient.adminUpdateUserAttributes {
+            it
+                .userPoolId(userPoolId)
+                .username(username)
+                .userAttributes({ builder ->
+                    builder
+                        .name(attribute)
+                        .value(value)
+                })
         }
     }
 
