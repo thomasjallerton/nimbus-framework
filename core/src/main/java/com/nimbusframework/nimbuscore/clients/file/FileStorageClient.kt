e2fa7804b2ecc3510ac79ed57f2f15463871f453
Thomas Allerton <thomasjallerton@gmail.com>
2023-01-21 18:21:26 +0000

    Add bytearray file storage option (#81)
    * Add bytearray file storage option
    
    * Use correct MD5 generation

--- core/src/main/java/com/nimbusframework/nimbuscore/clients/file/FileStorageClient.kt
+++ core/src/main/java/com/nimbusframework/nimbuscore/clients/file/FileStorageClient.kt
@@ -14,6 +14,9 @@ interface FileStorageClient {
     fun saveFile(path: String, content: String) = saveFile(path, content, mapOf())
     fun saveFile(path: String, content: String, tags: Map<String, String>)
 
+    fun saveFile(path: String, content: ByteArray) = saveFile(path, content, mapOf())
+    fun saveFile(path: String, content: ByteArray, tags: Map<String, String>)
+
     fun saveFileWithContentType(path: String, content: String, contentType: String) = saveFileWithContentType(path, content, contentType, mapOf())
     fun saveFileWithContentType(path: String, content: String, contentType: String, tags: Map<String, String>)
 
@@ -22,6 +25,9 @@ interface FileStorageClient {
 
     fun saveFileWithContentType(path: String, inputStream: InputStream, contentType: String) = saveFileWithContentType(path, inputStream, contentType, mapOf())
     fun saveFileWithContentType(path: String, inputStream: InputStream, contentType: String, tags: Map<String, String>)
+
+    fun saveFileWithContentType(path: String, content: ByteArray, contentType: String) = saveFileWithContentType(path, content, contentType, mapOf())
+    fun saveFileWithContentType(path: String, content: ByteArray, contentType: String, tags: Map<String, String>)
 
     fun deleteFile(path: String)
 
